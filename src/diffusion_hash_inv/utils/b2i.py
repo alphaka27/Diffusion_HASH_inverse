11137ee44358250f3156e3ffd314266256347de2
andy0000-droid <andy020207@gmail.com>
2025-11-26 16:43:00 +0900

    🍻 Temporary commit

--- src/diffusion_hash_inv/utils/b2i.py
+++ src/diffusion_hash_inv/utils/b2i.py
@@ -3,20 +3,63 @@ Byte string to image conversion utilities with EMNIST dataset.
 Input: Byte string in hex representation
 Output: Image
 """
+from pathlib import Path
+from typing import Optional
+
+from diffusion_hash_inv.utils import FileIO
+
 
 class ByteToImageConverter:
     """
     Converts byte strings to images using the EMNIST dataset.
     """
-    def __init__(self):
-        # Load EMNIST dataset or any other necessary resources
-        pass
+    def __init__(self, standalone: bool = False, **kwargs):
+        self.standalone = standalone
+
+        self.is_verbose: bool = kwargs.pop("is_verbose", not self.standalone)
+        self.length = kwargs.pop("length", 256)
+        self.file_io = FileIO(verbose_flag=self.is_verbose)
+
+        default_outuput_dir = self.file_io.select_data_dir(filetype="image", length=self.length)
+        data_path_arg = kwargs.pop("data_path", None)
+        self.data_path: Path = Path(data_path_arg) if data_path_arg else default_outuput_dir
+
+        default_json_dir = self.file_io.select_data_dir(filetype="json", length=self.length)
+        json_path_arg = kwargs.pop("json_path", None)
+        self.json_path: Path = Path(json_path_arg) if json_path_arg else default_json_dir
+        self.image_name: Optional[str] = None
+
+    def emnist_load(self):
+        """
+        EMNIST dataset loading function
+        
+        :param self: Description
+        """
 
     def byte_string_to_image(self, byte_string: str):
-        # Convert byte string to image using EMNIST dataset
-        pass
+        """
+        Converts a byte string to an image using the EMNIST dataset.
+        
+        :param self: Description
+        :param byte_string: Description
+        :type byte_string: str
+        """
+
+    def load_json_logs(self, json_file_path: str):
+        """
+        Loads JSON logs from a specified file path.
+        
+        :param self: Description
+        :param json_file_path: Description
+        :type json_file_path: str
+        """
 
     def main(self):
+        """
+        Main function to convert byte string to image.
+        
+        :param self: Description
+        """
         # Example usage
         byte_string = "example_byte_string"
         image = self.byte_string_to_image(byte_string)
